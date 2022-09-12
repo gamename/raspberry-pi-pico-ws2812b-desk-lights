c2e0ad4808ece4c48384603c177ad4990814dfed
gamename <tennis.n.smith@gmail.com>
2022-09-11 19:15:55 -0500

    initial commit

---
+++ lights.py
@@ -0,0 +1,33 @@
+"""
+"""
+import board
+import neopixel
+import RPi.GPIO as GPIO
+import time
+
+# How many pixels are in the WS2812b strip?
+MAX_PIXELS = 102
+
+# How bright should the LEDs be?
+BRIGHTNESS = 0.20  # 20%
+
+# Use the board internal definition for this
+LED_STRIP_OUTPUT_PIN = board.D18  # Physical pin 12
+
+# Colors
+BLUE = (0, 0, 255)
+OFF = (0, 0, 0)
+
+pixels = neopixel.NeoPixel(LED_STRIP_OUTPUT_PIN, MAX_PIXELS, brightness=BRIGHTNESS)
+
+GPIO.setwarnings(False)
+
+# Refer pins by their sequence number on the board
+GPIO.setmode(GPIO.BCM)
+
+while True:
+    try:
+        pixels.fill(BLUE)
+    except KeyboardInterrupt:
+        pixels.fill(OFF)
+        exit()
