134ea86c62c8aae6620ceb3f666703b7ac6ac4b3
gamename <tennis.n.smith@gmail.com>
2022-09-14 16:57:52 -0500

    add light sensor

--- boot.py
+++ boot.py
@@ -8,19 +8,29 @@ from machine import Pin
 # How many pixels do we have?
 PIXEL_COUNT = 172
 
+# How bright do we want to display?
+BRIGHTNESS = 0.5
+
 # Which pin controls the light strip?
 PIXEL_PIN = Pin(0)
+DARKNESS_DETECTION_PIN = 1
 
 # Define the color we want to use for the light strip
 BLUE = (0, 0, 255)
-
 OFF = (0, 0, 0)
 
 pixels = NeoPixel(PIXEL_PIN, PIXEL_COUNT)
+dark = Pin(DARKNESS_DETECTION_PIN, Pin.IN, Pin.PULL_DOWN)
+
+pixels.brightness = BRIGHTNESS
 
 pixels.fill(OFF)
 pixels.write()
 
 while True:
-    pixels.fill(BLUE)
-    pixels.write()
+    if dark.value():
+        pixels.fill(BLUE)
+        pixels.write()
+    else:
+        pixels.fill(OFF)
+        pixels.write()
